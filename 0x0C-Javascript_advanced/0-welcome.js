42837ef06e068b9171c969fb9e219f7783dbe3a3
cherni dhia <70884179+yonkodhia@users.noreply.github.com>
2023-05-23 13:54:36 +0100

    create a function

---
+++ 0x0C-Javascript_advanced/0-welcome.js
@@ -0,0 +1,6 @@
+function welcome (firstName, lastName) {
+  let fullName = firstName + ' ' + lastName;
+  function displayFullName () {
+    alert('Welcome' + ' ' + fullName + '!');}
+  displayFullName();
+}
